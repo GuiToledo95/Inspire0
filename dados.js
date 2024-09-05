92b9d8e7a83d62b47a9d58b33a0fcef34d6d0e70
GuiToledo95 <guiparolin@gmail.com>
2024-09-05 11:53:37 -0400

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,27 @@
+let dados = [
+    {
+        título: "Lavanda - Relaxa",
+        descrição: "Ajuda a melhorar o sono, amenizar os sintomas da enxaqueca, promove a calma, o bem-estar e a reduzir o estresse e a ansiedade.",
+        link: "https://www.instagram.com/candles.inspire/"
+    },
+    {
+        título: "Vanilla - Tranquiliza",
+        descrição: "É docemente aconchegante, elevando o bem-estar do ambiente. Seu lar ficará repleto de amor e boas energias.",
+        link: "https://www.instagram.com/candles.inspire/"
+    },
+    {
+        título: "Cereja & Avelã - Marca",
+        descrição: "Proporciona um aroma intenso e marcante no ambiente. Ajuda a criar uma atmosfera acolhedora e confortável, trazendo a sensação de bem-estar",
+        link: "https://www.instagram.com/candles.inspire/"
+    },
+    {
+        título: "Campim-Limão - Energiza",
+        descrição: "Ajuda a criar um ambiente harmonioso e positivo. O capim-limão limpa a mente, traz proteção e afasta a negatividade.",
+        link: "https://www.instagram.com/candles.inspire/"
+    },
+    {
+        título: "Camomila - Acalma",
+        descrição: "Com seu aroma quente e doce, proporciona um efeito emocional relaxante, ajudando a aliviar a tensão, raiva e medo, trazendo uma sensação de bem-estar e conforto.",
+        link: "https://www.instagram.com/candles.inspire/"
+    }
+];
